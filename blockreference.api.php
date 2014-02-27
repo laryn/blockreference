1822ec584fdb79130e5478174c479de2bb43431b
Rudie Dirkx <rudie@ezcompany.nl>
2014-02-27 16:07:13 +0100

    API to v2

--- blockreference.api.php
+++ blockreference.api.php
@@ -6,41 +6,26 @@
  */
 
 /**
- * Alter the potential references.
+ * Alter referenceable blocks.
  *
- * DEPRECATED in favor of hook_blockreference_potential_references2_alter().
+ * These are always block objects, not options. You can change the option label by changing $block->info.
  *
- * @param $references
- *  The references array to be modified.
- * @param $field
- *  Array containing field data.
- * @param $return_full_blocks
- *  Whether to return full blocks.
- * @param $string
- *  Filter string to match blocks.
- * @param $exact_string
- *  Strictly match string like for validation.
+ * For autocomplete fields, this alter happens AFTER the search, before slicing the results (ACDB does that).
+ *
+ * $context contains:
+ * - instance
+ * - type ("autocomplete" or "options_list", depending on where this list is requested)
+ * - entity
+ * - entity_type
  */
-function hook_blockreference_potential_references_alter(&$references, $field, $current_bids = array()) {
-  // no example code
-}
+function hook_blockreference_blocks_alter(&$blocks, $context) {
+  foreach ($blocks as $id => $block) {
+    // Add module & delta to pretty label.
+    $block->info .= ' (' . $block->module . '/' . $block->delta . ')';
 
-/**
- * Alter the potential references.
- *
- * @param $blocks
- *  List of referenceable blocks. Always keyed by bid. Might
- *  contain full block objects or only block titles, depending
- *  on `$context['return_full_blocks']`.
- * @param $context
- *  Assoc array containing:
- *  - field: Array: Array containing field data from field_info_field().
- *  - current_bids: Array: current field values.
- *  - return_full_blocks: Bool: Whether to return full blocks.
- *  - string: String: Filter string to match blocks.
- *  - exact_string: Bool: Strictly match string like for validation.
- */
-function hook_blockreference_potential_references2_alter(&$blocks, $context) {
-  $secret_bid = db_query('SELECT bid FROM {block} WHERE module = ? AND delta = ?', array('foo', 'bar'))->fetchField();
-  unset($blocks[$secret_bid]);
+    // Remove all Views blocks, except 'public_stuff'.
+    if ($block->module == 'views' && strpos($block->delta, 'public_stuff-') !== 0) {
+      unset($blocks[$id]);
+    }
+  }
 }
